ca7473fadf4cce50b1def88c9ee42ed7cd5cf4e3
Thor Wei Jie <thorweijie@yahoo.com>
2021-11-01 14:44:47 +0800

    Updated script to calculate separate WER

--- wer_calculator.py
+++ wer_calculator.py
@@ -5,8 +5,10 @@ import jiwer
 import numpy as np
 import stt
 
-ground_truth = []
-hypothesis = []
+ground_truth_products = []
+ground_truth_non_products = []
+hypothesis_products = []
+hypothesis_non_products = []
 
 
 # Text pre-processing (remove punctuation, lowercase everything and convert sentences to words) before evaluating WER
@@ -22,14 +24,17 @@ model.enableExternalScorer(scorer_path)
 
 # Create output csv file, read input csv file and write to output csv file
 with open("audio/results.csv", "w", newline="") as results_csv:
-    field_names = ["filename", "transcript", "coqui"]
+    field_names = ["filename", "transcript", "coqui", "contains_product"]
     writer = csv.DictWriter(results_csv, fieldnames=field_names)
     writer.writeheader()
     with open("audio/clips.csv", newline="") as clips_csv:
         reader = csv.DictReader(clips_csv)
         for row in reader:
             transcript = row["transcript"]
-            ground_truth.append(transcript)
+            contains_product = True if row["contains_product"] == "Y" else False
+            ground_truth_products.append(
+                transcript
+            ) if contains_product else ground_truth_non_products.append(transcript)
             filename = row["filename"]
             with wave.open(f"audio/{filename}", "rb") as wav:
                 audio = np.frombuffer(wav.readframes(wav.getnframes()), np.int16)
@@ -39,18 +44,29 @@ with open("audio/results.csv", "w", newline="") as results_csv:
                     "filename": f"{filename}",
                     "transcript": f"{transcript}",
                     "coqui": f"{text}",
+                    "contains_product": "Y" if contains_product else "N",
                 }
             )
-            hypothesis.append(text)
+            hypothesis_products.append(
+                text
+            ) if contains_product else hypothesis_non_products.append(text)
 
 # Apply text pre-processing and calculate WER
-result = jiwer.wer(
-    ground_truth,
-    hypothesis,
+result_products = jiwer.wer(
+    ground_truth_products,
+    hypothesis_products,
+    truth_transoform=transformation,
+    hypothesis_transform=transformation,
+)
+
+result_non_products = jiwer.wer(
+    ground_truth_non_products,
+    hypothesis_non_products,
     truth_transoform=transformation,
     hypothesis_transform=transformation,
 )
 
 # print(f"Ground truth: {ground_truth}")
 # print(f"Hypothesis: {hypothesis}")
-print(f"WER: {result}")
+print(f"WER (products): {result_products}")
+print(f"WER (non-products): {result_non_products}")
