40bc0fb191d2f8093a31a7f84ba156a8c715154b
Thor Wei Jie <thorweijie@yahoo.com>
2021-11-02 09:34:53 +0800

    Added normalise audio function

--- wer_calculator.py
+++ wer_calculator.py
@@ -1,6 +1,8 @@
 import csv
+from io import BytesIO
 import wave
 
+import ffmpeg
 import jiwer
 import numpy as np
 import stt
@@ -22,6 +24,27 @@ scorer_path = "coqui-stt-0.9.3-models.scorer"
 model = stt.Model(model_path)
 model.enableExternalScorer(scorer_path)
 
+
+# Convert audio to 16kHz sampling rate and 16-bit depth
+def normalize_audio(audio):
+    out, err = (
+        ffmpeg.input("pipe:0")
+        .output(
+            "pipe:1",
+            f="WAV",
+            acodec="pcm_s16le",
+            ac=1,
+            ar="16k",
+            loglevel="error",
+            hide_banner=None,
+        )
+        .run(input=audio, capture_stdout=True, capture_stderr=True)
+    )
+    if err:
+        raise Exception(err)
+    return out
+
+
 # Create output csv file, read input csv file and write to output csv file
 with open("audio/results.csv", "w", newline="") as results_csv:
     field_names = ["filename", "transcript", "coqui", "contains_product"]
@@ -36,8 +59,12 @@ with open("audio/results.csv", "w", newline="") as results_csv:
                 transcript
             ) if contains_product else ground_truth_non_products.append(transcript)
             filename = row["filename"]
-            with wave.open(f"audio/{filename}", "rb") as wav:
-                audio = np.frombuffer(wav.readframes(wav.getnframes()), np.int16)
+            with open(f"audio/{filename}", "rb") as file:
+                audio = file.read()
+                audio = normalize_audio(audio)
+                audio = BytesIO(audio)
+                with wave.open(audio) as wav:
+                    audio = np.frombuffer(wav.readframes(wav.getnframes()), np.int16)
             text = model.stt(audio)
             writer.writerow(
                 {
@@ -52,21 +79,25 @@ with open("audio/results.csv", "w", newline="") as results_csv:
             ) if contains_product else hypothesis_non_products.append(text)
 
 # Apply text pre-processing and calculate WER
-result_products = jiwer.wer(
-    ground_truth_products,
-    hypothesis_products,
-    truth_transoform=transformation,
-    hypothesis_transform=transformation,
-)
+if ground_truth_products and hypothesis_products:
+    result_products = jiwer.wer(
+        ground_truth_products,
+        hypothesis_products,
+        truth_transoform=transformation,
+        hypothesis_transform=transformation,
+    )
+    print(f"WER (products): {result_products}")
 
-result_non_products = jiwer.wer(
-    ground_truth_non_products,
-    hypothesis_non_products,
-    truth_transoform=transformation,
-    hypothesis_transform=transformation,
-)
+
+if ground_truth_non_products and hypothesis_non_products:
+    result_non_products = jiwer.wer(
+        ground_truth_non_products,
+        hypothesis_non_products,
+        truth_transoform=transformation,
+        hypothesis_transform=transformation,
+    )
+    print(f"WER (non-products): {result_non_products}")
+
 
 # print(f"Ground truth: {ground_truth}")
 # print(f"Hypothesis: {hypothesis}")
-print(f"WER (products): {result_products}")
-print(f"WER (non-products): {result_non_products}")
